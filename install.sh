8aacecf820ed95c6fd4cf5386a5c812308dd9c94
艾东阳 <60455096+XMGC666@users.noreply.github.com>
2020-06-24 11:18:32 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -317,9 +317,7 @@ config_caddy_docker_cloudflare(){
 
 # Install docker and docker compose
 install_docker(){
-    echo -e "开始安装 DOCKER "
-    curl -fsSL https://get.docker.com -o get-docker.sh
-    bash get-docker.sh
+  
     echo -e "开始安装 Docker Compose "
     curl -L https://github.com/docker/compose/releases/download/1.17.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
